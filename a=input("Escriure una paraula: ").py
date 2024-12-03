0432d2c8aa436409574701f9ff333149918a09a3
Ivanleon123 <imunyoz20974@iesjoanramis.org>
2024-12-03 08:42:39 +0100

    Add files via upload

---
+++ a=input("Escriure una paraula: ").py
@@ -0,0 +1,6 @@
+a=input("Escriure una paraula: ")
+b=input("Escriure una paraula: ")
+if len(a)>len(b):
+    print("la paraula {} es major que la {}".format(a,b))
+    print("f"la paraula {a} es major que la {b})
+    elif len(b)
